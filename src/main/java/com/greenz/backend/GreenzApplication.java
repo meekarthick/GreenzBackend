59afdf649d203dd162a0123f7f67288625906192
GreenzDevTeam <greenz.devteam@gmail.com>
2025-04-02 16:20:06 +0530

    INIT:base version-1

---
+++ src/main/java/com/greenz/backend/GreenzApplication.java
@@ -0,0 +1,13 @@
+package com.greenz.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreenzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GreenzApplication.class, args);
+    }
+
+}
